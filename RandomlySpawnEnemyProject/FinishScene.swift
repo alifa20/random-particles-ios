42897ebc1ca3991eb3700ff23698a190db5860ae
Samaneh Fathieh <samanehfathieh@Samanehs-MacBook-Pro.local>
2018-05-06 12:13:51 +1000

    error no

--- RandomlySpawnEnemyProject/FinishScene.swift
+++ RandomlySpawnEnemyProject/FinishScene.swift
@@ -8,23 +8,26 @@
 
 import SpriteKit
 
-struct ScoreRecord {
+struct ScoreRecord: Codable {
     let playerName: String
-    let score: String
-//    init(playerName: String, score: Double) {
-//        self.playerName = playerName
-//        self.score = score
-//    }
+    let score: Double
+    init(playerName: String, score: Double) {
+        self.playerName = playerName
+        self.score = score
+    }
 }
 
 class FinishScene: SKScene {
     override func didMove(to view: SKView) {
         let documentsDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
         print(documentsDir)
+//        let r = ScoreRecord(playerName: "jafar",score: 10.0)
+//        let records = [r]
+//        saveData(records: records)
     }
   
     func saveData(records: [ScoreRecord]) {
-        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
         let archiveURL = documentsDirectory.appendingPathComponent("high_scores").appendingPathExtension("json")
         do {
             let data = try JSONEncoder().encode(records)
@@ -34,16 +37,16 @@ class FinishScene: SKScene {
             print("Error saving data")
         }
     }
-    
-    func loadData() {
-        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let archiveURL = documentsDirectory.appendingPathComponent("high_scores").appendingPathExtension("json")
-        let jsonDecoder = JSONDecoder()
-        if let highScoresData = try? Data(contentsOf: archiveURL),
-            let decodedHighScores = try? jsonDecoder.decode([ScoreRecord].self, from: highScoresData) {
-            self.records = decodedHighScores
-            self.tableView.reloadData()
-        }
-    }
+//
+//    func loadData() {
+//        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
+//        let archiveURL = documentsDirectory.appendingPathComponent("high_scores").appendingPathExtension("json")
+//        let jsonDecoder = JSONDecoder()
+//        if let highScoresData = try? Data(contentsOf: archiveURL),
+//            let decodedHighScores = try? jsonDecoder.decode([ScoreRecord].self, from: highScoresData) {
+//            self.records = decodedHighScores
+//            self.tableView.reloadData()
+//        }
+//    }
     
 }
